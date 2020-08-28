5e5c86b6ed53e951248106bec87d27e9081d3a0f
ajayKumar99 <ajaxlegrands@gmail.com>
2020-08-28 22:26:09 +0530

    Booking ticket done

--- movieApi/routes.py
+++ movieApi/routes.py
@@ -5,6 +5,11 @@ from .extensions import db
 middleware = Blueprint('api', __name__, url_prefix='/api')
 api = Api(middleware)
 
+def convert_time(time):
+    hh, mm = time.split(':')
+    return hh.strip() + ':' + mm.strip()
+
+
 class BookMovieTicket(Resource):
     def post(self):
         req = request.get_json()
@@ -16,12 +21,29 @@ class BookMovieTicket(Resource):
         if 'timing' not in req:
             return {'error': 'Ticket timing not provided'}, 400
 
+        time_val = convert_time(req['timing'])
+        print(time_val)
+
         try:
+            timing_map = db.movie_tickets.timing_map
+            exists = timing_map.find_one({
+                'time': time_val
+            })
+            if exists:
+                if exists['bookings'] == 20:
+                    return {'error': 'Maximum booking limit for this time exeeded'}, 400
+            timing_map.update_one({
+                    'time': time_val
+                }, {
+                    '$inc': {
+                        'bookings': 1
+                    }
+                }, upsert=True)
             ticket_collection = db.movie_tickets.tickets
             ticket_id = ticket_collection.insert_one({
-                'name': req['name'],
-                'phone': req['phone'],
-                'timing': req['timing']
+                'name': req['name'].strip(),
+                'phone': req['phone'].strip(),
+                'timing': time_val
             })
         except Exception as e:
             return {'error': e.__str__()}, 500
