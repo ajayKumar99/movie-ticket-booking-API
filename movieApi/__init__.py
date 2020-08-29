21edafdbcbda542ceab992cebf504d2cc39ac1a9
ajayKumar99 <ajaxlegrands@gmail.com>
2020-08-29 16:57:09 +0530

    Test setup

--- movieApi/__init__.py
+++ movieApi/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask
 import os
 from .extensions import db
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
 
     try:
         os.makedirs(app.instance_path)
